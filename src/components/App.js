f0b4952be0826ee5fcfec099fda8240805a1a4cb
Mahsa Karim Fard <Mahsa.kf@yahoo.com>
2021-07-28 13:10:12 -0400

    Added fish component-displaying state with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
-import NotFound from './NotFound';
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+import { thisExpression } from '@babel/types';
 
 
 class App extends React.Component {
@@ -20,14 +22,24 @@ class App extends React.Component {
         // Set the new fishes object
         this.setState({fishes});
      };
+
+     loadSampleFishes = () => {
+         this.setState({ fishes : sampleFishes});
+     }
     render() {
         return (
             <div className="catch-of-the-day"> 
            <div className="menu">
             <Header tagline="Fresh Seafood Market" />
+            <ul className="fishes">
+                {Object.keys(this.state.fishes).map(
+                    key => <Fish key={key} details={this.state.fishes[key]}/>
+                    )}
+               
+            </ul>
            </div>
            <Order />
-           <Inventory addFish={this.addFish} />
+           <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
            
             </div>
         )
