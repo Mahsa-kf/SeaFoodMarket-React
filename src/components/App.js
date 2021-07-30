a88f424233ee1f1f223973162396b140cf9ba2b6
Mahsa Karim Fard <Mahsa.kf@yahoo.com>
2021-07-30 12:27:50 -0400

    Updated order State and displainf it with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
@@ -8,39 +8,56 @@ import { thisExpression } from '@babel/types';
 
 
 class App extends React.Component {
-     state = {
-         fishes: {},
-         order: {}
-     };
+    state = {
+        fishes: {},
+        order: {}
+    };
 
-     addFish = fish => {
-         
+    addFish = fish => {
+
         //take a copy of the exsitting state
-        const fishes = {...this.state.fishes};
+        const fishes = { ...this.state.fishes };
         //Add our new fish to that fishes variable.
-        fishes[`fish${ Date.now()}`] = fish;
+        fishes[`fish${Date.now()}`] = fish;
         // Set the new fishes object
-        this.setState({fishes});
-     };
+        this.setState({ fishes });
+    };
 
-     loadSampleFishes = () => {
-         this.setState({ fishes : sampleFishes});
-     }
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    };
+
+    addToOrder = (key) => {
+        // 1.take a copy of state
+        const order = { ...this.state.order };
+        // 2. Either add to the order, or update the number in our order
+        order[key] = order[key] + 1 || 1;
+
+        // 3. Call setState to update our state object
+        this.setState({ order });
+    }
+
+
     render() {
         return (
-            <div className="catch-of-the-day"> 
-           <div className="menu">
-            <Header tagline="Fresh Seafood Market" />
-            <ul className="fishes">
-                {Object.keys(this.state.fishes).map(
-                    key => <Fish key={key} details={this.state.fishes[key]}/>
-                    )}
-               
-            </ul>
-           </div>
-           <Order />
-           <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
-           
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => (
+                            <Fish
+                                 key={key}
+                                 index={key} 
+                                 details={this.state.fishes[key]}
+                                addToOrder={this.addToOrder} />
+                        ))}
+
+
+                    </ul>
+                </div>
+                <Order  fishes={this.state.fishes} order={this.state.order} />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
+
             </div>
         )
     }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -9,7 +9,7 @@ class StorePicker extends React.Component {
         // 1. Stop the form from submitting
         event.preventDefault();
         // 2. get the text from that input
-       const storeName =this.myInput.value;
+       const storeName =this.myInput.current.value;
         // 3. change the page to /store/whatever-they-entered
        this.props.history.push(`/store/${storeName}`);
     };
